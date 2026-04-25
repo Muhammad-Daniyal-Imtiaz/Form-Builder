487ce91ee665d4bb9e48215d5ebd819a2df6fd0e
Muhammad Daniyal Imtiaz <muhammaddaniimtiaz@gmail.com>
2026-04-25 17:42:48 +0500

    fix: official cloudflare asset configuration

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,31 +1,3 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-const config: OpenNextConfig = {
-  default: {
-    // Changing default to node to avoid bundling issues with auth routes
-    // Cloudflare nodejs_compat handles this perfectly
-    runtime: "node", 
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-  edgeExternals: ["node:crypto"],
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-};
-
-export default config;
+export default defineCloudflareConfig();
